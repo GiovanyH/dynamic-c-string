55c3c236c71f33615c7d1cb1e0354d13d61022d4
GiovanyH <56926698+GiovanyH@users.noreply.github.com>
2024-08-06 16:46:01 -0300

    Update dynamic_string.h

--- dynamic_string.h
+++ dynamic_string.h
@@ -3,62 +3,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <assert.h>
 
-typedef struct {
-    int len;
-    int cap;
-} array_header;
+#define vec_get_hdr(SB) ((int*)(SB) - 2)
+#define vec_push(SB, ELEM) ((void)((vec_get_hdr(SB)[0] >= vec_get_hdr(SB)[1]) ? \
+    (*(void **)&(SB) = (void*)((int*)realloc(vec_get_hdr(SB), 2 * sizeof(int) + (vec_get_hdr(SB)[1] *= 2) + 1) + 2)) : 0), \
+    (SB)[vec_get_hdr(SB)[0]++] = (ELEM))
+#define vec_pop(SB) ((void)((--vec_get_hdr(SB)[0] < vec_get_hdr(SB)[1] - 8) ? \
+    (*(void **)&(SB) = (void*)((int*)realloc(vec_get_hdr(SB), 2 * sizeof(int) + (vec_get_hdr(SB)[1] -= 8) + 1) + 2)) : 0))
 
-#define new_vector(TYPE, NAME) \
-    TYPE *NAME; \
-    array_header *HEADER; \
-    assert(&NAME != NULL); \
-    HEADER = malloc(sizeof(array_header) + 8); \
-    HEADER->cap = 8; \
-    HEADER->len = 0; \
-    NAME = (void*)(HEADER+1);
-
-#define vec_get_hdr(SB)      ((array_header *)SB-1)
-
-#define vec_get_len(SB)      (SB ? vec_get_hdr(SB)->len : 0)
-#define vec_get_capacity(SB) (SB ? vec_get_hdr(SB)->cap : 0)
-
-#define vec_push(SB, ELEM)   (vec_append_e((void**)&(SB), 1), (SB)[vec_get_len((SB))-1] = (ELEM))
-#define vec_pop(SB)          (vec_pop_e((void**)&(SB), 1))
-
-#define vec_free(SB)         (SB ? free(vec_get_hdr(SB)), 0 : 0)
-
-
-void vec_append_e(void** sb, size_t isz) {
-    array_header* h = vec_get_hdr(*sb);
-    if (h->len + 1 > h->cap) {
-        h->cap *= 2;
-        h = realloc(h, sizeof(array_header) + h->cap * isz);
-        *sb = h + 1;
+void append_string(char** sb, const char* string) {
+    while (*string) {
+        vec_push(*sb, *string++);
     }
-    h->len++;
+    (*sb)[vec_get_hdr(*sb)[0]] = '\0';
 }
 
-void vec_pop_e(void** sb, size_t isz) {
-    array_header* h;
+char* string_new(const char* string) {
+    char* p = (char*)((int*)malloc(2 * sizeof(int) + 8) + 2);
+    ((int*)p)[-2] = 0; ((int*)p)[-1] = 8;
 
-    h = vec_get_hdr(*sb);
-    if (h->len - 1 < h->cap - 8) {
-        h->cap -= 8;
-        h = realloc(h, sizeof(array_header) + h->cap * isz);
-        *sb = h + 1;
-    }
-    h->len--;
-}
+    append_string(&p, string);
 
-void append_string(char** sb, char* string) {
-    while (*string != '\0') {
-        vec_push(*sb, *string);
-        string++;
-    }
-    vec_push(*sb, '\0');
+    return p;
 }
 
 #endif
